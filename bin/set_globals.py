3b24e9a0a6dfaf446a81853f4efcc82d59f7679c
SuperStevePrice <SuperStevePrice@gmail.com>
2023-06-08 20:42:22 -0700

    Added bin/set_globals.py; revised bin/xt.py to import set_globals.

---
+++ bin/set_globals.py
@@ -0,0 +1,52 @@
+def set_globals():
+    global xtrc
+
+    xtrc = {
+        'x_rows': "24",
+        'x_cols': "80",
+        'x_fg': "black",
+        'x_bg': "grey",
+        'x_fa': "9x15bold",
+        'x_fs': "16",
+        'x_log': 0,
+		'x_sl': 200
+    }
+
+    # Read the .xtrc or ~/.xtrc file
+    try:
+        with open('.xtrc', 'r') as file:
+            config_lines = file.readlines()
+    except FileNotFoundError:
+        try:
+            with open(os.path.expanduser('~/.xtrc'), 
+                'r') as file:
+                config_lines = file.readlines()
+        except FileNotFoundError:
+            print("No .xtrc or ~/.xtrc file found.")
+            return
+
+    # Parse the configuration lines
+    #  and update the xtrc dictionary
+    for line in config_lines:
+        line = line.strip()
+        if line.startswith('#'):
+            continue
+
+        if line.startswith('x_cols'):
+            xtrc['x_cols'] = line.split('=')[1].strip()
+        elif line.startswith('x_rows'):
+            xtrc['x_rows'] = line.split('=')[1].strip()
+        elif line.startswith('x_sl'):
+            xtrc['x_sl'] = line.split('=')[1].strip()
+        elif line.startswith('x_fa'):
+            xtrc['x_fa'] = \
+                line.split('=')[1].strip().replace(' ', '')
+        elif line.startswith('x_fs'):
+            xtrc['x_fs'] = line.split('=')[1].strip()
+        elif line.startswith('x_bg'):
+            xtrc['x_bg'] = line.split('=')[1].strip()
+        elif line.startswith('x_fg'):
+            xtrc['x_fg'] = line.split('=')[1].strip()
+        elif line.startswith('x_log'):
+            xtrc['x_log'] = int(line.split('=')[1].strip())
+
--- bin/xt.py
+++ bin/xt.py
@@ -1,110 +1,9 @@
-#!/usr/bin/env python
-
-import time
-import subprocess
-import os
 import tkinter as tk
+import subprocess
+
+import xtrc
 
 
-def set_globals():
-    global x_cols
-    global x_rows
-    global x_sl
-    global x_bg
-    global x_fg
-    global x_fa
-    global x_fs
-    global x_log
-    global entry_cols
-    global frame
-    global frame_types
-    global top
-    global entry_font
-    global entry_font_size
-
-    entry_cols = define_entries()
-
-    # Read the .xtrc or ~/.xtrc file
-    try:
-        with open('.xtrc', 'r') as file:
-            config_lines = file.readlines()
-        except FileNotFoundError:
-            try:
-                with open(os.path.expanduser('~/.xtrc'), 'r') as file:
-                    config_lines = file.readlines()
-                except FileNotFoundError:
-                    print("No .xtrc or ~/.xtrc file found.")
-                    return
-
-                # Parse the configuration lines
-                for line in config_lines:
-                    line = line.strip()
-                    if line.startswith('#'):
-                        continue
-                    if line.startswith('x_cols'):
-                        x_cols = line.split( '=')[1].strip()
-                        elif line.startswith('x_rows'):
-                            x_rows = line.split( '=')[1].strip()
-                        elif line.startswith('x_sl'):
-                            x_sl = line.split( '=')[1].strip()
-                        elif line.startswith('x_fa'):
-                            x_fa = line.split( '=')[1].strip().replace(' ', '')
-                        elif line.startswith('x_fs'):
-                            x_fs = line.split( '=')[1].strip()
-                        elif line.startswith('x_log'):
-                            x_log = int( line.split('=')[1].strip())
-
-                    # Update the entry fields with the parsed values
-                    try:
-                        entry_cols.delete( 0, tk.END)
-                        entry_cols.insert( 0, x_cols)
-                        entry_rows.delete( 0, tk.END)
-                        entry_rows.insert( 0, x_rows)
-                        entry_font_size.delete( 0, tk.END)
-                        entry_font_size.insert( 0, x_sl)
-                        entry_font.delete( 0, tk.END)
-                        entry_font.insert( 0, x_fa)
-
-                        var_log.set( x_log) except NameError as e:
-                            print( "Error:", e)
-
-
-    def Xterm(x_bg, x_fg):
-    # Snatch the date from the scalar localtime():
-    date = time.strftime('%c')
-
-    # Create a window title from $env(LOGNAME), $host, and date:
-    title = f"{os.environ['LOGNAME']}@{'HOST'}   {date}"
-
-    # Create a geometry setting from columns and rows:
-    x_geo = f"{x_cols}x{x_rows}"
-
-        cmd = f"nohup /usr/bin/env xterm -ls -sb -sl {x_sl}"
-
-        if x_fa:
-            cmd += f" -fa \"{x_fa}\""
-
-        if x_fs:
-            cmd += f" -fs \"{x_fs}\""
-
-        if not x_fa:
-            # Parsing failed. Use fallback font.
-            cmd += " -fa \"9x15bold\""
-
-        if not x_fs:
-            # Parsing failed. Use fallback font.
-            cmd += " -fa 16"
-
-        cmd += f" -geometry {x_geo} -fg {x_fg} -bg {x_bg} -title \"{title}\""
-        cmd += " -l" if x_log else ""
-
-# Uncomment the line below to debug the cmd:
-# print(f"DEBUG: cmd={cmd}")
-
-subprocess.Popen(cmd, shell=True)
-
-
-def create_specific_Xterm_buttons():
 colors = [
     ("grey", "black"),
     ("LightSteelBlue", "navy"),
@@ -115,158 +14,82 @@ colors = [
     ("navy", "orange"),
     ("navy", "yellow"),
     ("DarkGreen", "white"),
-        ("DarkSeaGreen", "black"),
-        ("DarkRed", "white"),
-        ("salmon", "black"),
-        ("SaddleBrown", "white"),
-        ("tan", "black"),
-        ("LightYellow", "black"),
-        ("black", "lightSteelBlue")
-    ]
+    ("DarkSeaGreen", "black"),
+    ("DarkRed", "white"),
+    ("salmon", "black"),
+    ("SaddleBrown", "white"),
+    ("tan", "black"),
+    ("LightYellow", "black"),
+    ("black", "lightSteelBlue")
+]
 
-    frame_idx = 0
-    button_idx = 0
-    last_frame_idx = 0
-    button = []
-
-    for idx, (bg, fg) in enumerate(colors):
-        leading_zero = "0" if idx + 1 <= 9 else ""
-        button_text = f"Xterm Window {leading_zero}{idx + 1}"
-
-        if idx % 2 == 0:
-            frame_idx += 1
-
-        if last_frame_idx < frame_idx:
-            frame[frame_idx] = tk.Frame()
-            frame[frame_idx].pack(side="top", fill="y")
-            frame[frame_idx].configure(borderwidth=2, background="black")
-            last_frame_idx = frame_idx
-
-        button.append(tk.Button(frame[frame_idx], text=button_text, command=lambda bg=bg, fg=fg: Xterm(
-            bg, fg), borderwidth=5, padx=5, pady=5, background=bg, foreground=fg))
-        button[button_idx].pack(side="left")
-        button_idx += 1
+# Calculate the number of rows needed for centering
+num_rows = (len(colors) + 1) // 2
 
 
-def define_frames():
-    global frame
-    frame_types = ["colors", "sizes", "font", "font_button",
-                   "log", "custom_button", "quit_button"]
-    frame = {}
+xtrc_data = xtrc.set_globals()
+#print(xtrc_data)
 
-    for frame_type in frame_types:
-        frame[frame_type] = tk.Frame()
-        frame[frame_type].pack(side="top", fill="y")
-        frame[frame_type].configure(borderwidth=5, background="black")
+root = tk.Tk()
+root.geometry('500x600')
+root.title('Button color demo !!')
 
+# Set the background color of the root window to black
+root.configure(bg='black')
 
-def define_entries():
+def on_button_press(button):
+    button.config(bg=button.color_pair[0],
+                  fg=button.color_pair[1],
+                  highlightbackground=button.color_pair[0],
+                  highlightcolor=button.color_pair[0])
 
-    # Foreground Entry
-    global entry_fg
-    entry_fg = tk.Entry(frame["colors"], width=10)
-    entry_fg.insert(0, "black")
-    entry_fg.pack(side="left")
+    # Get the color values for xterm command
+    bg_color, fg_color = button.color_pair
 
-    # Background Entry
-    global entry_bg
-    entry_bg = tk.Entry(frame["colors"], width=10)
-    entry_bg.insert(0, "grey")
-    entry_bg.pack(side="left")
+    xterm_path = f"{xtrc_data['x_path']}/xterm"
+    cmd = f"{xtrc_data['x_path']}/xterm -sl {xtrc_data['x_sl']} " \
+        f"-geometry {xtrc_data['x_cols']}x{xtrc_data['x_rows']} " \
+        f"-fa {xtrc_data['x_fa']} -fs {xtrc_data['x_fs']} " \
+        f"-title \"Steve\'s Term Window\" -fg {fg_color} " \
+        f"-bg {bg_color}"
 
-    # Font Entry
-    global entry_font
-    entry_font = tk.Entry(frame["font"], width=30)
-    entry_font.insert(0, "9x15bold")
-    entry_font.pack(side="left")
+    if xterm_path is not None:
+        # xterm executable found
+        # Launch xterm with the specified colors and parameters
+        subprocess.Popen(cmd, shell=True)
 
-    # Font Size Entry
-    global entry_font_size
-    entry_font_size = tk.Entry(frame["sizes"], width=5)
-    entry_font_size.insert(0, "16")
-    entry_font_size.pack(side="left")
+    else:
+        # xterm not found
+        print("xterm is not installed or cannot be found in system PATH.")
 
+# Set the grid column weights to center the buttons
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
 
-def set_geometry():
-    global x_rows
-    global x_cols
+for i, color_pair in enumerate(colors):
+    button = \
+       tk.Button(root, text=f'Xterm Window {str(i + 1).zfill(2)}',
+       width=10, height=1, relief=tk.RAISED)
+    button.color_pair = color_pair
+    button.bind('<ButtonPress-1>', lambda event,
+                button=button: on_button_press(button))
 
-    x_rows = entry_rows.get()
-    x_cols = entry_cols.get()
+    # Calculate the row and column indices
+    row = i // 2
+    column = i % 2
 
+    button.grid(row=row, column=column, padx=5, pady=10,
+                sticky='nsew')
 
-def set_font_attributes():
-    global x_fa
-    global x_fs
+    # Set button colors
+    button.configure(bg=color_pair[0], fg=color_pair[1],
+                     highlightbackground=color_pair[0],
+                     highlightcolor=color_pair[0])
 
-    x_fa = entry_font.get()
-    x_fs = entry_font_size.get()
-
-
-def set_log_var():
-    global x_log
-
-    x_log = var_log.get()
-
-
-def destroy_window():
-    root.destroy()
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("XTerm Command Central")
-    define_frames()
-    define_entries()
-
-    # Rows
-    frame_rows = tk.Frame()
-    frame_rows.pack(side="top", fill="y")
-    frame_rows.configure(borderwidth=5, background="black")
-
-    label_rows = tk.Label(frame_rows, text="Rows:")
-    label_rows.pack(side="left")
-
-    global entry_rows
-    entry_rows = tk.Entry(frame_rows, width=5)
-    entry_rows.insert(0, "24")
-    entry_rows.pack(side="left")
-
-    # Columns
-    frame_cols = tk.Frame()
-    frame_cols.pack(side="top", fill="y")
-    frame_cols.configure(borderwidth=5, background="black")
-
-    label_cols = tk.Label(frame_cols, text="Columns:")
-    label_cols.pack(side="left")
-
-    global entry_cols
-    entry_cols = tk.Entry(frame_cols, width=5)
-    entry_cols.insert(0, "80")
-    entry_cols.pack(side="left")
-
-    # Font Button
-    button_font = tk.Button(frame["font_button"], text="Set Font",
-                            command=set_font_attributes, padx=5, pady=5)
-    button_font.pack(side="left")
-
-    # Log Checkbox
-    global var_log
-    var_log = tk.IntVar()
-    checkbox_log = tk.Checkbutton(
-        frame["log"], text="Log Session", variable=var_log, command=set_log_var)
-    checkbox_log.pack(side="left")
-
-    # Custom Button
-    button_custom = tk.Button(frame["custom_button"], text="Create Custom Xterm", command=lambda: Xterm(
-        entry_bg.get(), entry_fg.get()), borderwidth=5, padx=5, pady=5)
-    button_custom.pack(side="left")
-
-    # Quit Button
-    button_quit = tk.Button(frame["quit_button"], text="Quit",
-                            command=destroy_window, borderwidth=5, padx=5, pady=5)
-    button_quit.pack(side="left")
-
-    create_specific_Xterm_buttons()
+# Add a Quit button centered at the bottom of the root window
+quit_button = tk.Button(root, text='Quit', width=15, height=2,
+                        relief=tk.RAISED,
+                        command=root.destroy)
+quit_button.grid(row=num_rows, columnspan=2, pady=10)
 
 root.mainloop()
