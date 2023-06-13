d1ac6be4744fe0f6fccadcf462e805b0a435933e
SuperStevePrice <SuperStevePrice@gmail.com>
2023-06-13 16:56:36 -0700

    Revised dots/aliases, dots/profile.  Added xt.py, xtrc.py.

--- bin/xt.py
+++ bin/xt.py
@@ -1,124 +1,261 @@
-#!/usr/bin/env python
+#!/Users/steve/anaconda3/bin/python3.10
 
-import datetime as dt  # Rename the imported datetime module
-import os
-import tkinter as tk
+#-------------------------------------------------------------------------------
+#         Copyright (C) 2023    Steve Price    SuperStevePrice@gmail.com
+#
+#                       GNU GENERAL PUBLIC LICENSE
+#                        Version 3, 29 June 2007
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# PROGRAM:
+#   xt.py
+#   
+# PURPOSE:
+#   This Python program will create a dashboard to enable rapid xterm creation.
+#   
+# USAGE:
+#   xt.py
+#
+#   No inputs or outputs
+#-------------------------------------------------------------------------------
 import subprocess
-
-import xtrc
-from xtrc import report
-
-colors = [
-    ("grey", "black"),
-    ("LightSteelBlue", "navy"),
-    ("CadetBlue", "white"),
-    ("CadetBlue", "black"),
-    ("blue", "white"),
-    ("navy", "white"),
-    ("navy", "orange"),
-    ("navy", "yellow"),
-    ("DarkGreen", "white"),
-    ("DarkSeaGreen", "black"),
-    ("DarkRed", "white"),
-    ("salmon", "black"),
-    ("SaddleBrown", "white"),
-    ("tan", "black"),
-    ("LightYellow", "black"),
-    ("black", "lightSteelBlue")
-]
-
-# Calculate the number of rows needed for centering
-num_rows = (len(colors) + 1) // 2
+import os
+import sys
+import tkinter as tk
+from tkinter import messagebox
+import platform
 
 
-xtrc_data = xtrc.set_globals()
-#print("\nDEBUG:")
-#report()
+# Check the operating system
+if platform.system() == "Darwin":
+    try:
+        from tkmacosx import MacButton as Button
+    except ImportError:
+        from tkinter import Button
+else:
+    from tkinter import Button
 
+import xtrc as xt
+colors = xt.set_colors()
+xtrc = xt.set_xtrc()
 
+def on_button_click(bg, fg, custom_xterm):
+    """
+    Handle the spawning of xterm windows with desired attributes.
+
+    Parameters
+    ----------
+    bg, fg, custom_term
+    bg = bakground color
+    fg = foreground color
+    custom_xterm = booleen
+
+    Returns
+    -------
+    click : TYPE
+        DESCRIPTION.
+
+    """
+    def click():
+        print("DEBUG: ")
+        xtrc['x_bg'] = bg
+        xtrc['x_fg'] = fg
+        
+        try:
+            if custom_xterm:
+                xtrc['x_bg'] = "DarkGrey"
+                xtrc['x_fg'] = "Navy"
+            
+            cmd = xt.set_cmd(xtrc)
+            
+            print("Button clicked!")
+            print(cmd)
+            subprocess.Popen(cmd, shell=True)
+        except Exception as exception:
+            print("Error occurred while executing the command:", exception)
+
+    return click
+
+def enable_logging():
+    """
+    Turn xterm logging on and off.
+
+    Returns
+    -------
+    None.
+
+    """
+    print("DEBUG: enable_logging: x_log: ", xtrc['x_log'])
+    
+    if xtrc['x_log'] == 0:
+        xtrc['x_log'] = 1
+    
+    print("DEBUG: enable_logging: x_log: ", xtrc['x_log'])
+
+def quit_application():
+    """
+    Gracefully quit the application and free all resources.
+    """
+    if messagebox.askokcancel("Quit", "Are you sure you want to quit?"):
+        if 'spyder' in sys.modules:
+            os._exit(0)
+        else:
+            root.quit()
+
+def create_custom_xterm():
+    # Insert custom values here:
+    on_button_click(entry_bg, entry_fg, True)
+      
 root = tk.Tk()
-root.geometry('500x600')
-root.title('Xterm Dashboard')
-
-# Set the background color of the root window to black
 root.configure(bg='black')
+root.geometry('400x700')
+root.title('Color Buttons')
+custom_xterm = tk.BooleanVar(value=False)
 
-def on_button_press(button):
-    button.config(bg=button.color_pair[0],
-                  fg=button.color_pair[1],
-                  highlightbackground=button.color_pair[0],
-                  highlightcolor=button.color_pair[0])
 
-    # Get the color values for xterm command
-    bg_color, fg_color = button.color_pair
+# Create a frame to hold the buttons grid
+frame_buttons = tk.Frame(root, bg='black')
+frame_buttons.pack(padx=10, pady=10, anchor=tk.CENTER)
 
-    xterm_path = f"{xtrc_data['x_path']}/xterm"
-    enable_keystroke_logging = xtrc_data.get('x_log', '0')
+# Calculate the number of rows and columns for the buttons grid
+# Add 1 for the extra row if there are an odd number of buttons
+NUM_ROWS = (len(colors) + 1) // 2
+NUM_COLUMNS = 2
 
-    if enable_keystroke_logging != '0':
-        log = " -l "
-    else:
-        log = ""
-
-    # Build the xterm command
-    cmd =  \
-        xterm_path                                        + \
-        " -sb "                                           + \
-        " -sl "                                           + \
-        xtrc_data['x_sl']                                 + \
-        " -fa "                                           + \
-        f"'{xtrc_data['x_fa']}'"                          + \
-        " -fs "                                           + \
-        xtrc_data['x_fs']                                 + \
-        " -geometry "                                     + \
-        f"{xtrc_data['x_cols']}x{xtrc_data['x_rows']}"    + \
-        " -fg "                                           + \
-        fg_color                                          + \
-        " -bg "                                           + \
-        bg_color                                          + \
-        log                                               + \
-        " -title "                                        + \
-        f"'{os.environ['USER']}@{os.environ['HOME']} {dt.datetime.now()}'"
-
-    #print("\nDEBUG: cmd=", cmd)
-
-    if xterm_path is not None:
-        # xterm executable found
-        # Launch xterm with the specified colors and parameters
-        subprocess.Popen(cmd, shell=True)
-
-    else:
-        # xterm not found
-        print("xterm is not installed or cannot be found in system PATH.")
-
-# Set the grid column weights to center the buttons
-root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
-
+# Create the buttons grid
 for i, color_pair in enumerate(colors):
-    button = \
-       tk.Button(root, text=f'Xterm Window {str(i + 1).zfill(2)}',
-       width=10, height=1, relief=tk.RAISED)
-    button.color_pair = color_pair
-    button.bind('<ButtonPress-1>', lambda event,
-                button=button: on_button_press(button))
-
     # Calculate the row and column indices
-    row = i // 2
-    column = i % 2
+    row = i // NUM_COLUMNS
+    column = i % NUM_COLUMNS
 
-    button.grid(row=row, column=column, padx=5, pady=10,
-                sticky='nsew')
+    button = Button(frame_buttons,
+        text=f'Xterm Window {str(i + 1).zfill(2)}',
+        bg=color_pair[0], fg=color_pair[1],
+        activebackground="DarkGrey")
+    button.configure(
+        command=on_button_click(color_pair[0], color_pair[1], False))
+    button.grid(row=row, column=column, padx=4, pady=2, sticky=tk.EW)
 
-    # Set button colors
-    button.configure(bg=color_pair[0], fg=color_pair[1],
-                     highlightbackground=color_pair[0],
-                     highlightcolor=color_pair[0])
+# Create a frame to hold the additional elements
+frame_additional = tk.Frame(root, bg='black')
+frame_additional.pack(padx=10, pady=10, anchor=tk.CENTER)
 
-# Add a Quit button centered at the bottom of the root window
-quit_button = tk.Button(root, text='Quit', width=15, height=2,
-                        relief=tk.RAISED,
-                        command=root.destroy)
-quit_button.grid(row=num_rows, columnspan=2, pady=10)
+# A: Foreground and Background entry labels and boxes
+label_fg = tk.Label(frame_additional,
+            text="Foreground:", foreground="DarkGrey", background="black")
+label_fg.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
+entry_fg = tk.Entry(
+    frame_additional, textvariable=tk.StringVar(value=xtrc['x_fg']))
+entry_fg.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+
+label_bg = tk.Label(frame_additional, text="Background:",
+    foreground="DarkGrey", background="black")
+label_bg.grid(row=1, column=0, padx=5, pady=5, sticky=tk.E)
+entry_bg = tk.Entry(
+    frame_additional, textvariable=tk.StringVar(value=xtrc['x_bg']))
+entry_bg.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+
+# B: Cols, Rows, Font size labels and inputs
+label_cols = tk.Label(frame_additional, text="Cols:",
+    foreground="DarkGrey", background="black")
+label_cols.grid(row=2, column=0, padx=5, pady=5, sticky=tk.E)
+entry_cols = tk.Entry(
+    frame_additional, textvariable=tk.StringVar(value=xtrc['x_cols']))
+entry_cols.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
+
+label_rows = tk.Label(frame_additional, text="Rows:",
+    foreground="DarkGrey", background="black")
+label_rows.grid(row=3, column=0, padx=5, pady=5, sticky=tk.E)
+entry_rows = tk.Entry(
+    frame_additional, textvariable=tk.StringVar(value=xtrc['x_rows']))
+entry_rows.grid(row=3, column=1, padx=5, pady=5, sticky=tk.W)
+
+label_font_size = tk.Label(frame_additional,
+    text="Font Size:", foreground="DarkGrey", background="black")
+label_font_size.grid(row=4, column=0, padx=5, pady=5, sticky=tk.E)
+entry_font_size = tk.Entry(
+    frame_additional, textvariable=tk.StringVar(value=xtrc['x_fs']))
+entry_font_size.grid(row=4, column=1, padx=5, pady=5, sticky=tk.W)
+
+# C: Font Name entry box
+label_font_name = tk.Label(frame_additional, text="Font Name:",
+    foreground="DarkGrey", background="black")
+label_font_name.grid(row=5, column=0, padx=5, pady=5, sticky=tk.E)
+entry_font_name = tk.Entry(
+    frame_additional, textvariable=tk.StringVar(value=xtrc['x_fa']))
+entry_font_name.grid(row=5, column=1, padx=5, pady=5, sticky=tk.W)
+
+
+def populate_entry_boxes():
+    """
+    Fetch values from ~/.xtrc and populate input boxes.
+
+    Returns
+    -------
+    None.
+
+    """
+    home = os.path.expanduser("~")
+    xtrc_path = os.path.join(home, ".xtrc")
+
+    if os.path.isfile(xtrc_path):
+        with open(xtrc_path, 'r') as file:
+            xtrc_data = file.read()
+            xtrc_values = {}
+            for item in xtrc_data.split('\n'):
+                if item:
+                    pair = item.split('=')
+                    if len(pair) == 2:
+                        xtrc_values[pair[0]] = pair[1]
+
+        entry_fg.delete(0, tk.END)
+        entry_fg.insert(0, xtrc_values.get('x_fg', ''))
+
+        entry_bg.delete(0, tk.END)
+        entry_bg.insert(0, xtrc_values.get('x_bg', ''))
+
+        entry_cols.delete(0, tk.END)
+        entry_cols.insert(0, xtrc_values.get('x_cols', ''))
+
+        entry_rows.delete(0, tk.END)
+        entry_rows.insert(0, xtrc_values.get('x_rows', ''))
+
+        entry_font_size.delete(0, tk.END)
+        entry_font_size.insert(0, xtrc_values.get('x_fs', ''))
+
+        entry_font_name.delete(0, tk.END)
+        entry_font_name.insert(0, xtrc_values.get('x_fa', ''))
+
+
+# Call the function to populate the entry boxes
+populate_entry_boxes()
+
+# E: Enable Logging check box
+checkbox_logging = tk.Checkbutton(frame_additional,
+    text="Enable Logging", foreground="DarkGrey", background="black",
+    command=enable_logging)
+checkbox_logging.grid(row=7, column=0, columnspan=2,
+    padx=5, pady=5, sticky=tk.N)
+
+
+# F: Create Custom Font button
+button_create_custom_xterm = Button(frame_additional,
+    text="Create Custom Xterm",
+    command=create_custom_xterm(),
+        background=xtrc['x_bg'], foreground=xtrc['x_fg'])
+button_create_custom_xterm.configure(
+    command=on_button_click(xtrc['x_bg'], xtrc['x_fg'], True))
+button_create_custom_xterm.grid(
+    row=8, column=0, columnspan=2, padx=5, pady=5, sticky=tk.W + tk.E)
+
+# Create a frame to hold the Quit button
+frame_quit = tk.Frame(root, bg='black')
+frame_quit.pack(padx=10, pady=10, anchor=tk.CENTER)
+
+# G: Quit button
+button_quit = tk.Button(frame_quit, text="Quit", command=quit_application,
+                        bg="lightSteelBlue", fg="red")
+button_quit.pack(padx=5, pady=5, anchor=tk.CENTER)
 
 root.mainloop()
--- bin/xtrc.py
+++ bin/xtrc.py
@@ -1,10 +1,113 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+#-------------------------------------------------------------------------------
+#         Copyright (C) 2023    Steve Price    SuperStevePrice@gmail.com
+#
+#                       GNU GENERAL PUBLIC LICENSE
+#                        Version 3, 29 June 2007
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# PROGRAM:
+#   xtrc.py
+#   
+# PURPOSE:
+#   Python subroutines to be used by xt.py and any Python scripts that use the
+#   .xtrc environmental variables.
+#   
+# USAGE:
+#   import xtrc
+#-------------------------------------------------------------------------------
+import datetime as dt
 import os
 
-def set_globals():
-    global xtrc
+def set_cmd(xtrc):
+    """
+    set_cmd builds a legal xterm command using xtrc dictionary elements.
 
+    Parameters
+    ----------
+    xtrc : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    cmd : TYPE
+        DESCRIPTION.
+
+    """
+    xterm_executable = f"{xtrc['x_path']}/xterm"
+
+    # Build the xterm command
+    cmd =  \
+        xterm_executable + \
+        " -sb " + \
+        " -sl " + \
+        xtrc['x_sl'] + \
+        " -fa " + \
+        f"'{xtrc['x_fa']}'" + \
+        " -fs " + \
+        xtrc['x_fs'] + \
+        " -geometry " + \
+        f"{xtrc['x_cols']}x{xtrc['x_rows']}" + \
+        " -fg " + \
+        xtrc['x_fg'] + \
+        " -bg " + \
+        xtrc['x_bg'] + \
+        " -title " + \
+        f"'{os.environ['USER']}@{os.environ['HOME']} {dt.datetime.now()}'"
+
+    #print("DEBUG: set_cmd: Pre: ", cmd)
+
+    if xtrc['x_log'] == 1:
+        cmd += " -l"
+    #print("DEBUG: set_cmd: Post: ", cmd)
+
+    return cmd
+
+
+def set_colors():
+    """
+    set_colors defines background and foreground color pairs for xterm buttons.
+
+    Returns
+    -------
+    colors : TYPE
+        DESCRIPTION.
+
+    """
+    colors = [
+        ("grey", "black"),
+        ("LightSteelBlue", "navy"),
+        ("CadetBlue", "white"),
+        ("CadetBlue", "black"),
+        ("blue", "white"),
+        ("navy", "white"),
+        ("navy", "orange"),
+        ("navy", "yellow"),
+        ("DarkGreen", "white"),
+        ("DarkSeaGreen", "black"),
+        ("DarkRed", "white"),
+        ("salmon", "black"),
+        ("SaddleBrown", "white"),
+        ("tan", "black"),
+        ("LightYellow", "black"),
+        ("black", "lightSteelBlue")
+    ]
+    return colors
+
+
+def set_xtrc():
+    """
+    set_xtrc defines xtrc values by parsing the .xtrc file.
+
+    Returns
+    -------
+    xtrc : TYPE
+        DESCRIPTION.
+
+    """
+    # set defaults:
     xtrc = {
         'x_rows': "24",
         'x_cols': "80",
@@ -12,9 +115,9 @@ def set_globals():
         'x_bg': "grey",
         'x_fa': "9x15bold",
         'x_fs': "16",
-        'x_log': 0,
+        'x_log': 'None',
         'x_sl': 200,
-        'x_path': "",
+        'x_path': ""
     }
 
     # Read the .xtrc or ~/.xtrc file
@@ -54,14 +157,52 @@ def set_globals():
             xtrc['x_path'] = line.split('=')[1].strip()
         elif line.startswith('x_log'):
             xtrc['x_log'] = int(line.split('=')[1].strip())
+            xtrc['x_log'] = 0      # default: no logging
 
     return xtrc
 
-def report():
+
+def xtrc_values(xtrc):
+    """
+    xtrc_values is a debugging report for the xtrc dictionary.
+
+    Parameters
+    ----------
+    xtrc : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    print("\nxtrc:")
     for key, value in xtrc.items():
         print(f"{key}:\t{value}")
 
-if __name__ == "__main__":
-    xtrc = set_globals()
-    report()
 
+def colors_values(colors):
+    """
+    colors_values is a debugging report for the colors data structure.
+
+    Parameters
+    ----------
+    colors : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    print("\nColors:")
+    for idx, val in enumerate(colors):
+        print(f"{idx + 1}. {val}")
+
+
+if __name__ == "__main__":
+    colors = set_colors()
+    colors_values(colors)
+    xtrc = set_xtrc()
+    xtrc_values(xtrc)
+    print("\n", set_cmd(set_xtrc()))
